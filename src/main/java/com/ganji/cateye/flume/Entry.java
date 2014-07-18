3b5312b88f1465ada9687463d58877438429c7a8
chenyin <chenyin@ganji.com>
2014-07-18 18:27:25 +0800

    entry

--- src/main/java/com/ganji/cateye/flume/Entry.java
+++ src/main/java/com/ganji/cateye/flume/Entry.java
@@ -7,6 +7,12 @@ import java.util.Random;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.alibaba.rocketmq.client.exception.MQBrokerException;
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
+import com.alibaba.rocketmq.common.message.Message;
+import com.alibaba.rocketmq.remoting.exception.RemotingException;
+
 public class Entry {
 	private static final Logger logger = LoggerFactory.getLogger(Entry.class.getName());
 
@@ -15,12 +21,51 @@ public class Entry {
 			logger.warn("server starting");
 		}
 
-		int count = args != null && args.length > 0 ? Integer.parseInt(args[0]) : 1;
-		// sendlog(count);
-		// sendlogBackground(count);
+		int count = args != null && args.length > 0 ? Integer.parseInt(args[0]) : 10;
+		sendlogBackground(count);
 		System.out.println("done.");
-		
-		System.out.println(Entry.class.getCanonicalName());
+
+//		System.out.println(Entry.class.getCanonicalName());
+		// a(10);
+//		System.out.println("done 2. ");
+	}
+
+	private static void sink() {
+
+	}
+
+	private static void produce(int count) {
+		DefaultMQProducer producer = new DefaultMQProducer("cateye");
+		producer.setNamesrvAddr("192.168.129.213:9876");
+		// producer.setProducerGroup("cateye");
+		// producer.setInstanceName("CateyeProducer");
+		try {
+			producer.start();
+		} catch (MQClientException e) {
+			e.printStackTrace();
+		}
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<5000; i++){
+			sb.append("1");
+		};
+		String body = sb.toString();
+		try {
+			for (int i = 0; i < 1000; i++) {
+				Message m = new Message("cateye", "ms.web.app", body.getBytes());
+				producer.send(m);
+				Thread.sleep(10);
+			}
+		} catch (MQClientException e) {
+			e.printStackTrace();
+		} catch (RemotingException e) {
+			e.printStackTrace();
+		} catch (MQBrokerException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			producer.shutdown();
+		}
 	}
 
 	// private static void sendlogForkjoin(int logCount) {
@@ -32,7 +77,7 @@ public class Entry {
 			sendlog(logCount);
 			return;
 		}
-		int threadCount = 4;
+		int threadCount = 10;
 		final int batchSize = logCount / threadCount;
 		List<Thread> threads = new ArrayList<Thread>(threadCount);
 		for (int i = 0; i < threadCount; i++) {
@@ -55,20 +100,30 @@ public class Entry {
 		}
 	}
 
+	private static String body_;
+	
+	private static String body() {
+		if(body_ == null) {
+			int total = 102400;
+			StringBuilder sb = new StringBuilder();
+			sb.append("size=");
+			sb.append(total);
+			Random rnd = new Random();
+			while(sb.length() < total) {
+				sb.append(rnd.nextInt());
+			}
+			body_ = sb.toString();
+		}
+		return body_;
+	}
 	private static void sendlog(int count) {
 		int rnd = new Random().nextInt();
-		int midLen = 5000;
-		StringBuilder sb = new StringBuilder();
-		sb.append(" size=5k ");
-		for (int i = 0; i < midLen; i++) {
-			sb.append("-");
-		}
-		
+
 		for (int i = 1; i <= count; i++) {
-			if (i % 1000 == 0) {
-				System.out.println("flume test log, rnd=" + rnd + "count=" + i);
+			if (i % 100 == 0) {
+				System.out.println("flume test log, rnd=" + rnd + " count=" + i);
 			}
-			logger.warn("flume test log, rnd={} count={} {}", rnd, i, sb.toString());
+			logger.warn("flume test log, rnd={} count={} {}", rnd, i, body());
 		}
 	}
 
