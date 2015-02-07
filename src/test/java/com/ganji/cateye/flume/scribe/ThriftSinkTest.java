14a3cb0932ce13543fd672e9a2ca7bb86ba3d33f
chenyin <chenyin@ganji.com>
2015-02-07 14:30:55 +0800

    1. change to libthrift0.7 2. remove rocketmq 3. kestrel package shrink

---
+++ src/test/java/com/ganji/cateye/flume/scribe/ThriftSinkTest.java
@@ -0,0 +1,29 @@
+package com.ganji.cateye.flume.scribe;
+
+import static org.junit.Assert.*;
+
+import org.apache.flume.api.ThriftRpcClient;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ThriftSinkTest {
+
+	public ThriftSinkTest() {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void thriftRpcClientTest() {
+		
+		
+	}
+
+}
